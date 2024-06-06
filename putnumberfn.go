eff58e2b1bd4e74fb479d19ff3cbaa5c5a3ac5ed
myertaye <medetyertayev@gmail.com>
2024-06-07 04:24:06 +0600

    lost hope again

--- putnumberfn.go
+++ putnumberfn.go
@@ -23,4 +23,27 @@ func PutNumber(n int) {
 		ap.PutRune(rune(n/10 + 48))
 		ap.PutRune(rune(n%10 + 48))
 	}
+
+	if n < 0 && n > -10 {
+		ap.PutRune('-')
+		ap.PutRune(rune(n + 48))
+	} else if n <= -10 && n > -100 {
+		ap.PutRune('-')
+		ap.PutRune(rune(n/10 + 48))
+		ap.PutRune(rune(n%10 + 48))
+	} else if n <= -100 && n > -1000 {
+		ap.PutRune('-')
+		ap.PutRune(rune(n/100 + 48))
+		n = n - (n/100)*100
+		ap.PutRune(rune(n/10 + 48))
+		ap.PutRune(rune(n%10 + 48))
+	} else if n <= -1000 && n > -10000 {
+		ap.PutRune('-')
+		ap.PutRune(rune(n/1000 + 48))
+		n = n - (n/1000)*1000
+		ap.PutRune(rune(n/100 + 48))
+		n = n - (n/100)*100
+		ap.PutRune(rune(n/10 + 48))
+		ap.PutRune(rune(n%10 + 48))
+	}
 }
