9eba6e593b48e0df7b94b1d39e065ab42b90d68a
myertaye <medetyertayev@gmail.com>
2024-06-07 04:00:10 +0600

    put_number_fn with fmt

---
+++ putnumberfn.go
@@ -0,0 +1,21 @@
+package bootcamp
+
+import (
+	"fmt"
+)
+
+func PutNumber(n int) {
+	//n_length := 0
+	//
+	//for n > 0 {
+	//	n = n / 10
+	//	n_length++
+	//}
+
+	//for i := n_length; i > 0; i-- {
+	//	ap.PutRune(rune( n %  ))
+	//
+	//}
+
+	fmt.Print(n)
+}
