549cf6f41c8187527ced44c487a65f44ebe3c276
myertaye <medetyertayev@gmail.com>
2024-06-07 04:48:02 +0600

    put_number_fn with recursion

--- putnumberfn.go
+++ putnumberfn.go
@@ -5,45 +5,60 @@ import (
 )
 
 func PutNumber(n int) {
-	if n >= 0 && n < 10 {
-		ap.PutRune(rune(n + 48))
-	} else if n >= 10 && n < 100 {
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
-	} else if n >= 100 && n < 1000 {
-		ap.PutRune(rune(n/100 + 48))
-		n = n - (n/100)*100
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
-	} else if n >= 1000 && n < 10000 {
-		ap.PutRune(rune(n/1000 + 48))
-		n = n - (n/1000)*1000
-		ap.PutRune(rune(n/100 + 48))
-		n = n - (n/100)*100
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
-	}
+	//if n >= 0 && n < 10 {
+	//	ap.PutRune(rune(n + 48))
+	//} else if n >= 10 && n < 100 {
+	//	ap.PutRune(rune(n/10 + 48))
+	//	ap.PutRune(rune(n%10 + 48))
+	//} else if n >= 100 && n < 1000 {
+	//	ap.PutRune(rune(n/100 + 48))
+	//	n = n - (n/100)*100
+	//	ap.PutRune(rune(n/10 + 48))
+	//	ap.PutRune(rune(n%10 + 48))
+	//} else if n >= 1000 && n < 10000 {
+	//	ap.PutRune(rune(n/1000 + 48))
+	//	n = n - (n/1000)*1000
+	//	ap.PutRune(rune(n/100 + 48))
+	//	n = n - (n/100)*100
+	//	ap.PutRune(rune(n/10 + 48))
+	//	ap.PutRune(rune(n%10 + 48))
+	//}
+	//
+	//if n < 0 && n > -10 {
+	//	ap.PutRune('-')
+	//	ap.PutRune(rune((n)*(-1) + 48))
+	//} else if n <= -10 && n > -100 {
+	//	ap.PutRune('-')
+	//	ap.PutRune(rune((n/10)*(-1) + 48))
+	//	ap.PutRune(rune((n%10)*(-1) + 48))
+	//} else if n <= -100 && n > -1000 {
+	//	ap.PutRune('-')
+	//	ap.PutRune(rune((n/100)*(-1) + 48))
+	//	n = n - (n/100)*100
+	//	ap.PutRune(rune((n/10)*(-1) + 48))
+	//	ap.PutRune(rune((n%10)*(-1) + 48))
+	//} else if n <= -1000 && n > -10000 {
+	//	ap.PutRune('-')
+	//	ap.PutRune(rune((n/1000)*(-1) + 48))
+	//	n = n - (n/1000)*1000
+	//	ap.PutRune(rune((n/100)*(-1) + 48))
+	//	n = n - (n/100)*100
+	//	ap.PutRune(rune((n/10)*(-1) + 48))
+	//	ap.PutRune(rune((n%10)*(-1) + 48))
+	//}
+	//
+	//if (n < 0) {
+	//	ap.PutRune('-')
+	//	n = -n
+	//}
+	//Recursion(n)
+}
 
-	if n < 0 && n > -10 {
-		ap.PutRune('-')
-		ap.PutRune(rune((n)*(-1) + 48))
-	} else if n <= -10 && n > -100 {
-		ap.PutRune('-')
-		ap.PutRune(rune((n/10)*(-1) + 48))
-		ap.PutRune(rune((n%10)*(-1) + 48))
-	} else if n <= -100 && n > -1000 {
-		ap.PutRune('-')
-		ap.PutRune(rune((n/100)*(-1) + 48))
-		n = n - (n/100)*100
-		ap.PutRune(rune((n/10)*(-1) + 48))
-		ap.PutRune(rune((n%10)*(-1) + 48))
-	} else if n <= -1000 && n > -10000 {
-		ap.PutRune('-')
-		ap.PutRune(rune((n/1000)*(-1) + 48))
-		n = n - (n/1000)*1000
-		ap.PutRune(rune((n/100)*(-1) + 48))
-		n = n - (n/100)*100
-		ap.PutRune(rune((n/10)*(-1) + 48))
-		ap.PutRune(rune((n%10)*(-1) + 48))
+func Recursion(n int) {
+	if n < 10 {
+		ap.PutRune(rune(n + 48))
+		return
 	}
+	Recursion(n / 10)
+	ap.PutRune(rune(n%10 + 48))
 }
