d8b136451060ac60e171744994a238be95113f32
myertaye <medetyertayev@gmail.com>
2024-06-07 04:32:39 +0600

    :((((((((

--- putnumberfn.go
+++ putnumberfn.go
@@ -26,24 +26,24 @@ func PutNumber(n int) {
 
 	if n < 0 && n > -10 {
 		ap.PutRune('-')
-		ap.PutRune(rune(n + 48))
+		ap.PutRune(rune((n)*(-1) + 48))
 	} else if n <= -10 && n > -100 {
 		ap.PutRune('-')
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
+		ap.PutRune(rune((n/10)*(-1) + 48))
+		ap.PutRune(rune((n%10)*(-1) + 48))
 	} else if n <= -100 && n > -1000 {
 		ap.PutRune('-')
-		ap.PutRune(rune(n/100 + 48))
+		ap.PutRune(rune((n/100)*(-1) + 48))
 		n = n - (n/100)*100
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
+		ap.PutRune(rune((n/10)*(-1) + 48))
+		ap.PutRune(rune((n%10)*(-1) + 48))
 	} else if n <= -1000 && n > -10000 {
 		ap.PutRune('-')
-		ap.PutRune(rune(n/1000 + 48))
+		ap.PutRune(rune((n/1000)*(-1) + 48))
 		n = n - (n/1000)*1000
-		ap.PutRune(rune(n/100 + 48))
+		ap.PutRune(rune((n/100)*(-1) + 48))
 		n = n - (n/100)*100
-		ap.PutRune(rune(n/10 + 48))
-		ap.PutRune(rune(n%10 + 48))
+		ap.PutRune(rune((n/10)*(-1) + 48))
+		ap.PutRune(rune((n%10)*(-1) + 48))
 	}
 }
