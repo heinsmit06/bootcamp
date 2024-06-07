882bebb870c5958c1a6565a95a39c12aa62892b4
myertaye <medetyertayev@gmail.com>
2024-06-07 14:28:40 +0600

    fixed compare_them

--- comparethem.go
+++ comparethem.go
@@ -3,6 +3,8 @@ package bootcamp
 func CompareThem(a, b *int) bool {
 	if a == nil && b == nil {
 		return true
+	} else if (a == nil && b != nil) || (a != nil && b == nil) {
+		return false
 	}
 
 	var a_value int = *a
