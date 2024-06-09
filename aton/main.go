42a40e3b19cf95455e03a552ec77fb356c416959
myertaye <medetyertayev@gmail.com>
2024-06-09 19:59:05 +0600

    aton

--- aton/main.go
+++ aton/main.go
@@ -14,7 +14,26 @@ func main() {
 	fmt.Scanf("%s", &str)
 
 	for i := 0; i < string_length; i++ {
-		bootcamp.PutNumber(int(rune(str[i])))
+
+		if str[i] == '\t' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == '\n' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == '\v' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == '\f' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == '\r' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == ' ' {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == 0x85 {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else if str[i] == 0xA0 {
+			bootcamp.PutNumber(int(rune(str[i])))
+		} else {
+			bootcamp.PutNumber(int(rune(str[i])))
+		}
 
 		if i < len(str)-1 {
 			ap.PutRune(' ')
