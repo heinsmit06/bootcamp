cfaf202f9f78d66c7c04666dd4dbeb7536cbc2d9
myertaye <medetyertayev@gmail.com>
2024-06-09 18:19:37 +0600

    test array print

--- test_dir/main.go
+++ test_dir/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bootcamp"
+	"fmt"
+)
 
 func main() {
+	arr := [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	bootcamp.ArrayPrint(arr)
 	var n int
 	fmt.Scanf("%d", &n)
 
