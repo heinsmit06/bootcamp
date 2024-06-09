5a5d8f2e5727244db1c79bb51d35e3800396b43c
myertaye <medetyertayev@gmail.com>
2024-06-09 16:38:58 +0600

    added additional conditions to check if a pointer is nil

--- unpackit.go
+++ unpackit.go
@@ -1,8 +1,9 @@
 package bootcamp
 
 func UnpackIt(p *******int) int {
-	if p == nil {
+	if p == nil || *p == nil || **p == nil || ***p == nil || ****p == nil || *****p == nil || ******p == nil {
 		return 0
 	}
+
 	return *******p
 }
