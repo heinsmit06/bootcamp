067789a58b3d45d3f07899e3f25cb38d69b8100c
myertaye <medetyertayev@gmail.com>
2024-06-09 18:14:40 +0600

    test array print

--- arrayprint.go
+++ arrayprint.go
@@ -1,14 +1,12 @@
 package bootcamp
 
 import (
-	"bootcamp"
-
 	"github.com/alem-platform/ap"
 )
 
 func ArrayPrint(arr [20]int) {
 	for i := 0; i < len(arr); i++ {
-		bootcamp.PutNumber(arr[i])
+		PutNumber(arr[i])
 
 		if i < len(arr)-1 {
 			ap.PutRune(' ')
