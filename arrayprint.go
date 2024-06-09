620e1b012d6bddcd7718fa510b500aca7dbb05db
myertaye <medetyertayev@gmail.com>
2024-06-09 18:11:52 +0600

    test array print

---
+++ arrayprint.go
@@ -0,0 +1,15 @@
+package bootcamp
+
+import (
+	"github.com/alem-platform/ap"
+)
+
+func ArrayPrint(arr [20]int) {
+	for i := 0; i < len(arr); i++ {
+		bootcamp.PutNumber(arr[i])
+
+		if i < len(arr)-1 {
+			ap.PutRune(' ')
+		}
+	}
+}
