e17d31b7b910be7c1d9756ed9ce6bc01de500f25
myertaye <medetyertayev@gmail.com>
2024-06-09 18:36:35 +0600

    Added new PutNumber1 and Recursion1 to avoid redefinition

--- arrayprint.go
+++ arrayprint.go
@@ -6,10 +6,27 @@ import (
 
 func ArrayPrint(arr [20]int) {
 	for i := 0; i < len(arr); i++ {
-		PutNumber(arr[i])
+		PutNumber1(arr[i])
 
 		if i < len(arr)-1 {
 			ap.PutRune(' ')
 		}
 	}
+}
+
+func PutNumber1(n int) {
+	if n < 0 {
+		ap.PutRune('-')
+		n = -n
+	}
+	Recursion(n)
+}
+
+func Recursion1(n int) {
+	if n < 10 {
+		ap.PutRune(rune(n + 48))
+		return
+	}
+	Recursion(n / 10)
+	ap.PutRune(rune(n%10 + 48))
 }
