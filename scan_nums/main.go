4dae362fcdfd718fe918daef33f8c4f4b34e4ffb
myertaye <medetyertayev@gmail.com>
2024-06-09 17:02:35 +0600

    scan nums and do algebraic operations with them

---
+++ scan_nums/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bootcamp"
+	"fmt"
+
+	"github.com/alem-platform/ap"
+)
+
+func main() {
+	var int1 int
+	var int2 int
+	fmt.Scanf("%d %d", &int1, &int2)
+
+	bootcamp.PutNumber(int1 + int2)
+	ap.PutRune(' ')
+	bootcamp.PutNumber(int1 - int2)
+	ap.PutRune(' ')
+	bootcamp.PutNumber(int1 * int2)
+	ap.PutRune(' ')
+
+	if int2 != 0 {
+		bootcamp.PutNumber(int1 / int2)
+	} else {
+		ap.PutRune('F')
+	}
+
+	ap.PutRune('\n')
+}
