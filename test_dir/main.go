1d91b3214914c8482b3084d7dd0b27c1d4b5f062
myertaye <medetyertayev@gmail.com>
2024-06-09 18:31:05 +0600

    asd

--- test_dir/main.go
+++ test_dir/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	arr := [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	arr := [20]int{1, -2, 3, 4, 5, -6, 0, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	bootcamp.ArrayPrint(arr)
 	var n int
 	fmt.Scanf("%d", &n)
