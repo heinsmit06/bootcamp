eec40f2832d84e0fce5c11c465ac8b6c05f186d0
myertaye <medetyertayev@gmail.com>
2024-06-09 16:19:47 +0600

    added additional conditions regarding nil pointers

--- swap.go
+++ swap.go
@@ -1,6 +1,12 @@
 package bootcamp
 
 func Swap(a, b *int) {
+	if a == nil && b == nil {
+		return
+	} else if (a == nil && b != nil) || (a != nil && b == nil) {
+		return
+	}
+
 	var temp int
 	temp = *a
 	*a = *b
