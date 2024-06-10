813ce376c867920477f630fd12891274f32462ff
myertaye <medetyertayev@gmail.com>
2024-06-10 17:50:33 +0600

    returns slice depending on a low and high indexes

--- arrayslice.go
+++ arrayslice.go
@@ -1,15 +1,19 @@
 package bootcamp
 
 func ArraySlice(arr [20]int, low int, high int) []int {
-	if (low < high) && (low >= 0 && high < 20) {
-		slc := make([]int, high-low)
+	if (low < high) && (low >= 0 && high <= 20) {
+
+		slice_length := high - low + 1
+		slc := make([]int, slice_length)
 		slc_counter := 0
+
 		for i := low; i < high; i++ {
 			slc[slc_counter] = arr[i]
 			slc_counter++
 		}
 
 		return slc
+
 	} else {
 		slc := []int{}
 		return slc
