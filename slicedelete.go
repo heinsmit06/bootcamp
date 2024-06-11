ef1bb0c7c4f620f2ff0e028e143b6769a49cc00a
myertaye <medetyertayev@gmail.com>
2024-06-12 03:35:19 +0600

    removes elements from the slice between low and high indexes, inclusive and exclusive respectively

--- slicedelete.go
+++ slicedelete.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package bootcamp
 
 func SliceDelete(arr *[]int, low, high int) bool {
 	if (low < 0) || (high > len(*arr)) || !(low < high) || (*arr == nil) {
@@ -9,27 +7,8 @@ func SliceDelete(arr *[]int, low, high int) bool {
 
 	arr_len := len(*arr)
 	arr_copy := *arr
+
 	copy((*arr)[low:], arr_copy[high:])
 	(*arr) = (*arr)[:(arr_len - (high - low))]
 	return true
-}
-
-func main() {
-	arr := []int{0, 1, 2, 3, 4, 5}
-
-	fmt.Println(arr)                     // [0, 1, 2, 3, 4, 5]
-	fmt.Println(SliceDelete(&arr, 1, 3)) // true
-	fmt.Println(arr)                     // [0, 3, 4, 5]
-
-	fmt.Println(SliceDelete(&arr, 0, 2)) // false
-	fmt.Println(arr)                     // [0, 3, 4, 5]
-
-	fmt.Println(SliceDelete(&arr, 5, 3)) // false
-	fmt.Println(arr)                     // [0, 3, 4, 5]
-
-	fmt.Println(SliceDelete(&arr, -10, 5)) // false
-	fmt.Println(arr)                       // [0, 3, 4, 5]
-
-	fmt.Println(SliceDelete(&arr, 0, 2)) // true
-	fmt.Println(arr)                     // []
 }
