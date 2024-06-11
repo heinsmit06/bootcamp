42b751a7d117b9d7876a2033fcea877b0bbbfa2d
myertaye <medetyertayev@gmail.com>
2024-06-12 05:43:45 +0600

    bubble sort

---
+++ slicesort.go
@@ -0,0 +1,16 @@
+package bootcamp
+
+func SliceSort(arr *[]int) {
+	arr_length := len(*arr)
+	temp := 0
+
+	for i := 0; i < arr_length-1; i++ {
+		for j := 0; j < arr_length-i-1; j++ {
+			if (*arr)[j] > (*arr)[j+1] {
+				temp = (*arr)[j]
+				(*arr)[j] = (*arr)[j+1]
+				(*arr)[j+1] = temp
+			}
+		}
+	}
+}
