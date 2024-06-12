2997eb990c77db9493b9485f41f101792af2df0a
myertaye <medetyertayev@gmail.com>
2024-06-12 07:43:49 +0600

    rotate matrix clockwise

---
+++ slicematrixrotateclockwise.go
@@ -0,0 +1,15 @@
+package bootcamp
+
+func SliceMatrixRotateClockwise(matrix [][]rune) {
+	// reverse the matrix
+	for i, j := 0, len(matrix)-1; i < j; i, j = i+1, j-1 {
+		matrix[i], matrix[j] = matrix[j], matrix[i]
+	}
+
+	// transpose it
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
