2ee6864c5683b2fea51c702b58a2b3dc41d699e4
myertaye <medetyertayev@gmail.com>
2024-06-15 01:55:59 +0600

    iterative power of a number

--- powiterative.go
+++ powiterative.go
@@ -4,7 +4,7 @@ func PowIterative(x int, power int) int {
 	if power < 0 {
 		return -1
 	} else if power == 0 {
-		return x
+		return 1
 	}
 
 	result := 1
@@ -13,3 +13,12 @@ func PowIterative(x int, power int) int {
 	}
 	return result
 }
+
+//func main() {
+//	fmt.Println(PowIterative(2, -1)) // -1
+//	fmt.Println(PowIterative(2, 0))  // 1
+//	fmt.Println(PowIterative(2, 1))  // 2
+//	fmt.Println(PowIterative(2, 2))  // 4
+//	fmt.Println(PowIterative(2, 3))  // 8
+//	fmt.Println(PowIterative(2, 4))  // 16
+//}
