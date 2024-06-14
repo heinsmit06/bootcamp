f9ba2e697c70b253f39ff7b2cac5a5c8f76537dc
myertaye <medetyertayev@gmail.com>
2024-06-14 12:56:56 +0600

    converts int to str

--- itoa.go
+++ itoa.go
@@ -1,10 +1,22 @@
 package bootcamp
 
-import (
-	"strconv"
-)
-
 func Itoa(n int) string {
-	var str string = strconv.Itoa(n)
-	return str
+	if n == 0 {
+		return "0"
+	}
+
+	var result string
+	sign := ""
+	if n < 0 {
+		sign = "-"
+		n = -n
+	}
+
+	for n > 0 {
+		digit := n % 10
+		result = string(rune(digit+48)) + result
+		n /= 10
+	}
+
+	return sign + result
 }
