43a77af664dd4752ac4f46cc61de3d5149cafcf2
myertaye <medetyertayev@gmail.com>
2024-06-20 20:44:31 +0600

    finds empty files or files consisting only of spaces and newlines

--- print_program_name/main.go
+++ print_program_name/main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/alem-platform/ap"
 )
 
 func main() {
-	args := os.Args[0]
-
-	var n int
-	for i := len(args) - 1; i >= 0; i-- {
-		if args[i] == '.' || args[i] == '/' {
-			n = i + 1
-			break
-		}
-	}
-	str := args[n:]
-
-	for _, char := range str {
-		ap.PutRune(char)
-	}
+	args := os.Args
+	fmt.Print(args[0])
 	ap.PutRune('\n')
 }
+
+///////////////// NOT MINE
+
+//func main() {
+//	args := os.Args[0]
+//
+//	var n int
+//	for i := len(args) - 1; i >= 0; i-- {
+//		if args[i] == '.' || args[i] == '/' {
+//			n = i + 1
+//			break
+//		}
+//	}
+//	str := args[n:]
+//
+//	for _, char := range str {
+//		ap.PutRune(char)
+//	}
+//	ap.PutRune('\n')
+//}
--- test_dir/main.go
+++ test_dir/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	///////////////                MAPS MAPS MAPS MAPS MAPS MAPS MAPS MAPS
@@ -24,4 +27,14 @@ func main() {
 	val, ok := m1["Sanzhar"]
 	fmt.Printf("val: %v\n", val)
 	fmt.Printf("ok: %v\n", ok)
+
+	///////////////                os.Args, Work with files
+
+	args := os.Args
+	fmt.Printf("Program name: %v\n", args[0])
+	fmt.Printf("args: %s\n", args[1:])
+	filename := os.Args[1]
+	fmt.Printf("filename: %s\n", filename)
+	content, _ := os.ReadFile(filename)
+	fmt.Println(string(content))
 }
