a44f56b07e8083d214ec9c68064ff54257c821a6
myertaye <medetyertayev@gmail.com>
2024-06-21 05:01:37 +0600

    my version of print program arguments

--- print_program_arguments/main.go
+++ print_program_arguments/main.go
@@ -7,13 +7,22 @@ import (
 )
 
 func main() {
-	args := os.Args[1:] // Exclude the first argument which is the program name
-
-	for _, arg := range args {
-		// Output each argument followed by a newline
-		for _, char := range arg {
-			ap.PutRune(char)
+	args := os.Args
+	for i := 1; i < len(args); i++ {
+		for _, v := range args[i] {
+			ap.PutRune(rune(v))
 		}
-		ap.PutRune('\n')
 	}
 }
+
+//func main() {
+//	args := os.Args[1:] // Exclude the first argument which is the program name
+//
+//	for _, arg := range args {
+//		// Output each argument followed by a newline
+//		for _, char := range arg {
+//			ap.PutRune(char)
+//		}
+//		ap.PutRune('\n')
+//	}
+//}
