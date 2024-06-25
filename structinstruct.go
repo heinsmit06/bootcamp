db7b1b12d87c81095337458b1704627be0785514
myertaye <medetyertayev@gmail.com>
2024-06-25 22:27:02 +0600

    storing an object of struct User into another struct declaration

---
+++ structinstruct.go
@@ -0,0 +1,17 @@
+package bootcamp
+
+import "bootcamp/firststruct"
+
+type Cart struct {
+	Owner *firststruct.User
+	Items []string
+}
+
+func CreateMyCart(owner *firststruct.User, items []string) *Cart {
+	myCart := Cart{
+		Owner: owner,
+		Items: items,
+	}
+
+	return &myCart
+}
