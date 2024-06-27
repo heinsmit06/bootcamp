8412fc39d3befb502fa4c72fcfd638fe516cd224
myertaye <medetyertayev@gmail.com>
2024-06-28 03:39:59 +0600

    removes the given node from the doubly linked list

--- list/remove.go
+++ list/remove.go
@@ -1,16 +1,20 @@
 package list
 
 func (l *List) Remove(n *ListNode) {
+	if l.Head == n {
+		l.Head = nil
+		return
+	}
+
 	current := l.Head
 
-	if current.Next != nil {
-		for current.Next != n {
-			current = current.Next
-		}
-		if n.Next != nil {
-			current.Next = n.Next
-		} else {
-			current.Next = nil
-		}
+	for current.Next != n && current.Next != nil {
+		current = current.Next
+	}
+
+	if n.Next != nil {
+		current.Next = n.Next
+	} else {
+		current.Next = nil
 	}
 }
