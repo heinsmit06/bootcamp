38a0f0372aa985614aa5505ee31ae70e02a1cf49
myertaye <medetyertayev@gmail.com>
2024-06-28 07:03:38 +0600

    method iterates through the list and return the first node for which the provided function returns true. If no such node is found, it returns nil

---
+++ list/find.go
@@ -0,0 +1,18 @@
+package list
+
+func (l *List) Find(fn func(v interface{}) bool) *ListNode {
+	if l.Head == nil {
+		return nil
+	}
+
+	current := l.Head
+	for current.Next != nil {
+		if fn(current) {
+			return current
+		}
+
+		current = current.Next
+	}
+
+	return nil
+}
